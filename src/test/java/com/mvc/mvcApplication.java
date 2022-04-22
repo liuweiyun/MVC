27037a5284af4e666e7e6ff0ee2955eec395cbd7
lweiyun <1019362982@qq.com>
2022-04-22 21:19:11 +0800

    Initial commit

---
+++ src/test/java/com/mvc/mvcApplication.java
@@ -0,0 +1,9 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+
+public class mvcApplication {
+    public static void main(String[] args){
+        SpringApplication.run(mvcApplication.class,args);
+    }
+}
